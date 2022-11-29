2a5e87562959ddc4e3ec3b2d86e0e8345f80a0f3
salimsensussoft <salim.murshed@sensussoft.com>
2022-11-29 17:51:26 +0600

    update: fetching all data, components from api
    fetching all data, components from api

--- lib/presentation/ui/association_request_details_screen/association_request_details_screen_view_model.dart
+++ lib/presentation/ui/association_request_details_screen/association_request_details_screen_view_model.dart
@@ -1,1 +1,227 @@
-import 'package:bingo_wholesale/services/navigation/navigationService.dart';import 'package:stacked/stacked.dart';import '../../../app/locator.dart';import '../../../services/auth_service/auth_service.dart';import '../../widgets/alert/activation_dialog.dart';class AssociationRequestDetailsScreenModel extends BaseViewModel {  final AuthService _authService = locator<AuthService>();  final NavigationService _navigationService = locator<NavigationService>();  bool get isRetailer => _authService.isRetailer.value;  void openActivationCodeDialog() {    _navigationService.displayDialog(const ActivationDialog());  }  List<String> selectionDataList = [    'Item 1',    'Item 2',    'Item 3',    'Item 4',    'Item 5',  ];  String selectedCustomerType = "Item 1";  String selectedGracePeriodGroups = "Item 1";  String selectedPricingGroups = "Item 1";  String selectedSalesZone = "Item 1";  String selectedAllowOrders = "Item 1";  String selectedCustomerSinceDate = "Item 1";  String selectedVisitFrequency = "Item 1";  void changeCustomerType(String data) {    selectedCustomerType = data;    notifyListeners();  }  void changeGracePeriodGroups(String data) {    selectedGracePeriodGroups = data;    notifyListeners();  }  void changePricingGroups(String data) {    selectedPricingGroups = data;    notifyListeners();  }  void changeSalesZone(String data) {    selectedSalesZone = data;    notifyListeners();  }  void changeAllowOrders(String data) {    selectedAllowOrders = data;    notifyListeners();  }  void changeCustomerSinceDate(String data) {    selectedCustomerSinceDate = data;    notifyListeners();  }  void changeVisitFrequency(String data) {    selectedVisitFrequency = data;    notifyListeners();  }  void gotoBackScreen() {    _navigationService.pop();  }}
+import 'package:bingo_wholesale/repository/repository_components.dart';
+import 'package:bingo_wholesale/repository/repository_retailer.dart';
+import 'package:bingo_wholesale/services/navigation/navigationService.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:stacked/stacked.dart';
+
+import '../../../app/locator.dart';
+import '../../../const/app_strings.dart';
+import '../../../data_models/models/association_wholesaler_equest_details_model/association_wholesaler_equest_details_model.dart';
+import '../../../data_models/models/component_models/sales_zone_model.dart';
+import '../../../data_models/models/component_models/tax_id_type_model.dart';
+import '../../../data_models/models/retailer_wholesaler_association_request_model/retailer_wholesaler_association_request_model.dart';
+import '../../../repository/repository_wholesaler.dart';
+import '../../../services/auth_service/auth_service.dart';
+import '../../widgets/alert/activation_dialog.dart';
+import '../../widgets/alert/date_picker.dart';
+
+class AssociationRequestDetailsScreenModel extends ReactiveViewModel {
+  AssociationRequestDetailsScreenModel() {}
+
+  //locator service
+  final AuthService _authService = locator<AuthService>();
+  final NavigationService _navigationService = locator<NavigationService>();
+  final RepositoryWholesaler _repositoryWholesaler =
+      locator<RepositoryWholesaler>();
+  final RepositoryRetailer _repositoryRetailer = locator<RepositoryRetailer>();
+  final RepositoryComponents _repositoryComponents =
+      locator<RepositoryComponents>();
+
+  //get methods
+  bool get isRetailer => _authService.isRetailer.value;
+  bool get setScreenBusy => isRetailer
+      ? _repositoryRetailer.setScreenBusy.value
+      : _repositoryWholesaler.setScreenBusy.value;
+  AssociationWholesalerRequestDetailsModel
+      get associationRequestWholesalerDetails =>
+          _repositoryWholesaler.associationRequestWholesalerDetails.value;
+  RetailerAssociationRequestDetailsModel
+      get associationRequestRetailerDetails =>
+          _repositoryRetailer.associationRequestRetailerDetails;
+  InternalInformation get internalInformation => _repositoryWholesaler
+      .associationRequestWholesalerDetails
+      .value
+      .data![0]
+      .internalInformation![0];
+  CreditlineInformation get creditlineInformation => _repositoryWholesaler
+      .associationRequestWholesalerDetails
+      .value
+      .data![0]
+      .creditlineInformation!;
+
+  //local variables
+  String selectedCustomerType = "Select ${AppString.selectCustomerType}";
+  String selectedGracePeriodGroups = "Select ${AppString.gracePeriodGroups}";
+  String selectedPricingGroups = "Select ${AppString.pricingGroups}";
+  String selectedSalesZoneString = "xxxxxx";
+  String selectedAllowOrders = "Select ${AppString.allowOrders}";
+  String selectedCustomerSinceDate = "Select ${AppString.customerSinceDate}";
+  int selectedVisitFrequency = 0;
+  int status = 0;
+  String uniqueId = "";
+
+  //local text controllers
+  TextEditingController internalIdController = TextEditingController();
+  TextEditingController monthlySalesController = TextEditingController();
+  TextEditingController averageSalesTicketController = TextEditingController();
+  TextEditingController suggestedCreditLineController = TextEditingController();
+  TextEditingController selectDate = TextEditingController();
+
+  //get service data list
+  TaxIdType get taxIdType => _repositoryComponents.taxIdType;
+  List<String> get customerType => _repositoryComponents.customerType.data!
+      .map((e) => e.customerType!)
+      .toList();
+  List<String> get gracePeriodGroup =>
+      _repositoryComponents.gracePeriodGroup.data!
+          .map((e) => e.gracePeriodGroup!)
+          .toList();
+  List<String> get pricingGroup => _repositoryComponents.pricingGroup.data!
+      .map((e) => e.pricingGroups!)
+      .toList();
+  List<SalesZoneModelData> get salesZone =>
+      _repositoryComponents.salesZone.data!;
+  List<String> allowOrdersList = ["Yes", "No"];
+  List<dynamic> visitFrequentlyList = [
+    {"id": 1, "title": "Twice a week"},
+    {"id": 2, "title": "Weekly"},
+    {"id": 3, "title": "Every two weeks"},
+    {"id": 4, "title": "Every Three Weeks"},
+    {"id": 5, "title": "Monthly"}
+  ];
+
+  int getStatus(String status) {
+    switch (status.toLowerCase()) {
+      case "accepted":
+        return 1;
+      case "rejected":
+        return 2;
+      case "verified":
+        return 3;
+      case "active":
+        return 4;
+      default:
+        return 0;
+    }
+  }
+
+  void presetFunction() {
+    internalIdController.text = internalInformation.internalId!;
+    monthlySalesController.text = creditlineInformation.monthlySales!;
+    averageSalesTicketController.text =
+        creditlineInformation.averageSalesTicket!;
+    suggestedCreditLineController.text =
+        creditlineInformation.suggestedCreditlineAmount!;
+    selectDate.text =
+        DateFormat('dd/MM/yyyy').format(DateTime.now()).toString();
+    selectedCustomerType = internalInformation.customerType!.isEmpty
+        ? selectedCustomerType
+        : internalInformation.customerType!;
+    selectedGracePeriodGroups = selectedGracePeriodGroups;
+    selectedAllowOrders = internalInformation.allowOrders == 0 ? "No" : "Yes";
+    selectedPricingGroups = internalInformation.pricingGroup!.isEmpty
+        ? selectedPricingGroups
+        : internalInformation.pricingGroup!;
+    // selectedSalesZoneString = internalInformation.salesZone == null
+    //     ? selectedSalesZoneString
+    //     : internalInformation.salesZone!;
+    // print('creditlineInformation.visitFrequency');
+    // print(internalInformation.salesZone);
+    selectedVisitFrequency = creditlineInformation.visitFrequency == null
+        ? selectedVisitFrequency
+        : creditlineInformation.visitFrequency!;
+    status = getStatus(_repositoryWholesaler.associationRequestWholesalerDetails
+        .value.data![0].companyInformation![0].status!);
+    notifyListeners();
+  }
+
+  void openActivationCodeDialog() {
+    _navigationService.displayDialog(const ActivationDialog());
+  }
+
+  void callDetails(String arguments) async {
+    uniqueId = arguments;
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      isRetailer
+          ? _repositoryRetailer.getRetailerAssociationDetails(arguments)
+          : await _repositoryWholesaler
+              .getWholesalersAssociationDetails(arguments);
+      presetFunction();
+    });
+  }
+
+  void openCalender() async {
+    selectDate.text = (DateFormat('dd/MM/yyyy')
+            .format(await _navigationService.animatedDialog(DatePicker())))
+        .toString();
+    notifyListeners();
+  }
+
+  void gotoBackScreen() {
+    _navigationService.pop();
+  }
+
+  void changeCustomerType(String data) {
+    selectedCustomerType = data;
+    notifyListeners();
+  }
+
+  void changeGracePeriodGroups(String data) {
+    selectedGracePeriodGroups = data;
+    notifyListeners();
+  }
+
+  void changePricingGroups(String data) {
+    selectedPricingGroups = data;
+    notifyListeners();
+  }
+
+  void changeSalesZone(String data) {
+    selectedSalesZoneString = data;
+    print(selectedSalesZoneString);
+    notifyListeners();
+  }
+
+  void changeAllowOrders(String data) {
+    selectedAllowOrders = data;
+    notifyListeners();
+  }
+
+  void changeCustomerSinceDate(String data) {
+    selectedCustomerSinceDate = data;
+    notifyListeners();
+  }
+
+  void changeVisitFrequency(int data) {
+    selectedVisitFrequency = data;
+    notifyListeners();
+  }
+
+  void updateStatus() {
+    var data = {
+      "unique_id": uniqueId,
+      "action": status,
+    };
+    var dataForStatus4 = {
+      "unique_id": uniqueId,
+      "action": 4,
+      "internal_id": internalIdController.text,
+      "customer_type": selectedCustomerType,
+      "grace_period_group": selectedGracePeriodGroups,
+      "pricing_group": selectedPricingGroups,
+      "sales_zone_unique_id": selectedSalesZoneString,
+      "allow_orders": selectedAllowOrders,
+      "customer_since_date": selectDate.text,
+      "monthly_sales": monthlySalesController.text,
+      "average_sales_ticket": averageSalesTicketController.text,
+      "visit_frequency": selectedVisitFrequency,
+      "suggested_creditline_amount": suggestedCreditLineController.text
+    };
+  }
+
+  @override
+  List<ReactiveServiceMixin> get reactiveServices =>
+      [_repositoryWholesaler, _repositoryRetailer, _repositoryComponents];
+}
--- lib/services/network/network_urls.dart
+++ lib/services/network/network_urls.dart
@@ -1,6 +1,7 @@
 import '../../app/app_config.dart';
 
 class NetworkUrls {
+  NetworkUrls._();
   static Uri loginUrl = Uri.parse("${Config.baseURL}login");
   static Uri countryUrl = Uri.parse("${Config.baseURL}country-list");
   static Uri storeUrl = Uri.parse("${Config.baseURL}store-list");
@@ -18,4 +19,18 @@ class NetworkUrls {
       "${Config.baseURL}retailer-wholesaler-association-request-list");
   static Uri requestAssociationListForWholesaler = Uri.parse(
       "${Config.baseURL}wholesaler-retailer-association-request-list");
+  static Uri viewWholesalerRetailerAssociationRequest = Uri.parse(
+      "${Config.baseURL}view-wholesaler-retailer-association-request");
+  static Uri viewRetailerWholesalerAssociationRequest = Uri.parse(
+      "${Config.baseURL}view-retailer-wholesaler-association-request");
+
+  //components urls
+
+  static Uri taxIdType = Uri.parse("${Config.baseURL}tax-id-type-list");
+  static Uri customerType = Uri.parse("${Config.baseURL}customer-type-list");
+  static Uri gracePeriodGroup =
+      Uri.parse("${Config.baseURL}grace-period-groups-list");
+  static Uri pricingGroup = Uri.parse("${Config.baseURL}pricing-groups-list");
+  static Uri salesZone =
+      Uri.parse("${Config.baseURL}wholesaler-sales-zone-list");
 }
